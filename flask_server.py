833ed9747aab1cae81e90e83c68e52a1fe68fd4b
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-11-13 07:41:14 -0500

    Removed commented codes

--- flask_server.py
+++ flask_server.py
@@ -2,19 +2,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 import subprocess
-import os
-
-#import classify
-
 
 @app.route("/")
-# def runprogram():
-#     # coll_run = subprocess.run(
-#     #     [program, *args],
-#     #     input=inputstr.encode(),
-#     #     stdout=subprocess.PIPE,
-#     #     stderr=subprocess.PIPE)
-#     coll_run = subprocess.call('python', 'classify.py', shell=False)
 def upload_files():
     return render_template('tensor.html')
 
@@ -24,11 +13,7 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
-        # runprogram()
-        # classify.run_inference_on_image('panda.JPG')
         output = subprocess.run("python classify.py --image_file " + f.filename, shell=True)
-        #return (output)
-        #return test
         return 'file uploaded successfully'
 
 
